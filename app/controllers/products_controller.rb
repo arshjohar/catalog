ccf411673010cc7e2e39415e52a48be051cbdab5
Arshneet Singh <arsh.johar@gmail.com>
2013-12-11 18:50:58 +0530

    some UI fixes related to flash display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   def index
     if params[:gtin]
       @product = Product.find_with_gtin(params[:gtin])
-      flash[:error] = "Product with GTIN #{params[:gtin]} does not exist." unless @product
+      flash.now[:error] = "Product with GTIN #{params[:gtin]} does not exist." unless @product
     end
   end
 
